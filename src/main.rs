ed408929427815cff5543611e276fbe035dbe08e
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-01-17 19:19:01 +0100

    add swagger ui

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,81 @@ extern crate rocket;
 use rocket::http::Status;
 use rocket::response::{content, status};
 use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, JsonSchema)]
 #[serde(crate = "rocket::serde")]
 struct InputPath {
     input_path: String,
 }
-#[derive(Serialize)]
+#[derive(Serialize, JsonSchema)]
 #[serde(crate = "rocket::serde")]
 struct Paths {
-    output_paths: Vec<String>,
+    paths_list: Vec<String>,
 }
 impl Paths {
     pub fn from_listdir(input_path: Json<InputPath>) -> Paths {
         Paths {
-            output_paths: utils::get_list_dir(&input_path.input_path[..]),
+            paths_list: utils::get_list_dir(&input_path.input_path[..]),
         }
     }
     pub fn from_glob(input_path: Json<InputPath>) -> Paths {
         Paths {
-            output_paths: utils::get_glob(&input_path.input_path[..]),
+            paths_list: utils::get_glob(&input_path.input_path[..]),
         }
     }
     pub fn from_walk(input_path: Json<InputPath>) -> Paths {
         Paths {
-            output_paths: utils::get_walk(&input_path.input_path[..]),
+            paths_list: utils::get_walk(&input_path.input_path[..]),
         }
     }
 }
+#[openapi(tag = "Default")]
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
-
+#[openapi(tag = "Fun")]
 #[get("/coffe")]
 fn coffe() -> status::Custom<content::RawJson<&'static str>> {
     status::Custom(Status::ImATeapot, content::RawJson("{ \"hi\": \"world\" }"))
 }
+#[openapi(tag = "FileSystem")]
 #[post("/listdir", format = "application/json", data = "<input_path>")]
 fn flistdir(input_path: Json<InputPath>) -> Json<Paths> {
     Json(Paths::from_listdir(input_path))
 }
-
+#[openapi(tag = "FileSystem")]
 #[post("/glob", format = "application/json", data = "<input_path>")]
 fn fglob(input_path: Json<InputPath>) -> Json<Paths> {
     Json(Paths::from_glob(input_path))
 }
-
+#[openapi(tag = "FileSystem")]
 #[post("/walk", format = "application/json", data = "<input_path>")]
 fn fwalk(input_path: Json<InputPath>) -> Json<Paths> {
     Json(Paths::from_walk(input_path))
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, flistdir, fglob, fwalk, coffe])
+#[rocket::main]
+async fn main() {
+    let launch_result = rocket::build()
+        .mount(
+            "/",
+            openapi_get_routes![index, flistdir, fglob, fwalk, coffe],
+        )
+        .mount(
+            "/docs/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .launch()
+        .await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
 }
