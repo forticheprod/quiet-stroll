7d185820ba968e59c1b1b2a9762386f612d9e13b
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-06-23 15:49:07 +0200

    re-organise test and line

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,10 @@ pub fn get_walk(input_path: &str) -> Vec<String> {
         .map(|x| x.path().display().to_string())
         .collect()
 }
+#[test]
+fn test_get_walk() {
+    assert_eq!(9, get_walk("./samples/").len());
+}
 
 pub fn get_list_dir(input_path: &str) -> Vec<String> {
     fs::read_dir(input_path)
@@ -16,6 +20,10 @@ pub fn get_list_dir(input_path: &str) -> Vec<String> {
         .map(|x| x.path().display().to_string())
         .collect()
 }
+#[test]
+fn test_get_list_dir() {
+    assert_eq!(7, get_list_dir("./samples").len());
+}
 pub fn get_glob(input_path: &str) -> Vec<String> {
     glob(input_path)
         .unwrap()
@@ -23,14 +31,7 @@ pub fn get_glob(input_path: &str) -> Vec<String> {
         .map(|x| x.display().to_string())
         .collect()
 }
-#[test]
-fn test_get_walk() {
-    assert_eq!(9, get_walk("./samples/").len());
-}
-#[test]
-fn test_get_list_dir() {
-    assert_eq!(7, get_list_dir("./samples").len());
-}
+
 #[test]
 fn test_get_glob() {
     assert_eq!(5, get_glob("./samples/*.tif").len());
