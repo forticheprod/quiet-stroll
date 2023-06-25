36aaecd16da0d8c7da93c556d229bf8961caec99
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-06-25 16:27:18 +0200

    add unit test and add a sort to the walk

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ mod test {
         let client = Client::tracked(rocket()).expect("valid rocket instance");
         let response = client.post("/walk").json(&message).dispatch();
         assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap().replace("\\\\", "/"), "{\"paths_list\":[\"./samples\",\"./samples/aaa.002.tif\",\"./samples/aaa.005.tif\",\"./samples/aaa.001.tif\",\"./samples/aaa.004.tif\",\"./samples/aaa.003.tif\",\"./samples/bbb.001.exr\",\"./samples/subfolder\",\"./samples/subfolder/ccc.050.exr\"]}");
+        assert_eq!(response.into_string().unwrap().replace("\\\\", "/"), "{\"paths_list\":[\"./samples/aaa.001.tif\",\"./samples/aaa.002.tif\",\"./samples/aaa.003.tif\",\"./samples/aaa.004.tif\",\"./samples/aaa.005.tif\",\"./samples/bbb.001.exr\",\"./samples/subfolder\",\"./samples/subfolder/ccc.050.exr\",\"./samples\"]}");
     }
     #[test]
     fn test_glob() {
@@ -76,6 +76,6 @@ mod test {
         let client = Client::tracked(rocket()).expect("valid rocket instance");
         let response = client.post("/glob").json(&message).dispatch();
         assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap().replace("\\\\", "/"),"{\"paths_list\":[\"samples\\\\aaa.001.tif\",\"samples\\\\aaa.002.tif\",\"samples\\\\aaa.003.tif\",\"samples\\\\aaa.004.tif\",\"samples\\\\aaa.005.tif\"]}");
+        assert_eq!(response.into_string().unwrap().replace("\\\\", "/"),"{\"paths_list\":[\"samples/aaa.001.tif\",\"samples/aaa.002.tif\",\"samples/aaa.003.tif\",\"samples/aaa.004.tif\",\"samples/aaa.005.tif\"]}");
     }
 }
