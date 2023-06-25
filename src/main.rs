f5a5d3a1369126b56d1fe90a7910fd96faf4f9f4
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-06-25 16:46:51 +0200

    fix with path detection

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use rocket::http::Status;
 use rocket::response::{content, status};
 use rocket::serde::json::Json;
 use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
+#[cfg(test)]
+mod tests;
 
 #[openapi(tag = "Default")]
 #[get("/")]
@@ -46,36 +48,4 @@ fn rocket() -> _ {
                 ..Default::default()
             }),
         )
-}
-
-#[cfg(test)]
-mod test {
-    use super::rocket;
-    use quiet_stroll::InputPath;
-    use rocket::http::Status;
-    use rocket::local::blocking::Client;
-
-    #[test]
-    fn hello_world() {
-        let client = Client::tracked(rocket()).expect("valid rocket instance");
-        let response = client.get(uri!(super::index)).dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap(), "Hello, world!");
-    }
-    #[test]
-    fn test_walk() {
-        let message = InputPath::new("./samples".to_string());
-        let client = Client::tracked(rocket()).expect("valid rocket instance");
-        let response = client.post("/walk").json(&message).dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap().replace("\\\\", "/"), "{\"paths_list\":[\"./samples\",\"./samples/aaa.001.tif\",\"./samples/aaa.002.tif\",\"./samples/aaa.003.tif\",\"./samples/aaa.004.tif\",\"./samples/aaa.005.tif\",\"./samples/bbb.001.exr\",\"./samples/subfolder\",\"./samples/subfolder/ccc.050.exr\"]}");
-    }
-    #[test]
-    fn test_glob() {
-        let message = InputPath::new("./samples/*.tif".to_string());
-        let client = Client::tracked(rocket()).expect("valid rocket instance");
-        let response = client.post("/glob").json(&message).dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap().replace("\\\\", "/"),"{\"paths_list\":[\"samples/aaa.001.tif\",\"samples/aaa.002.tif\",\"samples/aaa.003.tif\",\"samples/aaa.004.tif\",\"samples/aaa.005.tif\"]}");
-    }
 }
