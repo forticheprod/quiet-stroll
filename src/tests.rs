97c6e6f1ac1a61f60d8884e732f2186ddcb4e392
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-07-07 11:12:44 +0200

    add comments

--- src/tests.rs
+++ src/tests.rs
@@ -4,6 +4,7 @@ use rocket::http::Status;
 use rocket::local::blocking::Client;
 use std::env;
 
+/// Simple test of the hello_world
 #[test]
 fn hello_world() {
     let client = Client::tracked(rocket()).expect("valid rocket instance");
@@ -11,6 +12,7 @@ fn hello_world() {
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.into_string().unwrap(), "Hello, world!");
 }
+/// Test the walk function
 #[test]
 fn test_walk() {
     let message = InputPath::new("./samples".to_string());
@@ -24,6 +26,7 @@ fn test_walk() {
         assert_eq!(response.into_string().unwrap().replace("\\\\", "/"), "{\"paths_list\":[\"./samples/aaa.001.tif\",\"./samples/aaa.002.tif\",\"./samples/aaa.003.tif\",\"./samples/aaa.004.tif\",\"./samples/aaa.005.tif\",\"./samples/bbb.001.exr\",\"./samples/subfolder\",\"./samples/subfolder/ccc.050.exr\",\"./samples\"]}");
     }
 }
+/// Test the listdir function
 #[test]
 fn test_listdir() {
     let message = InputPath::new("./samples/".to_string());
@@ -32,6 +35,7 @@ fn test_listdir() {
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.into_string().unwrap().replace("\\\\", "/"),"{\"paths_list\":[\"./samples/aaa.001.tif\",\"./samples/aaa.002.tif\",\"./samples/aaa.003.tif\",\"./samples/aaa.004.tif\",\"./samples/aaa.005.tif\",\"./samples/bbb.001.exr\",\"./samples/subfolder\"]}");
 }
+/// Test the glob function
 #[test]
 fn test_glob() {
     let message = InputPath::new("./samples/*.tif".to_string());
